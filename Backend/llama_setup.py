1aca93f47e04c5f3c15f2747c96a68cbaa72223a
Shreyaan Pradhan <shreyaan.pradhan2020@vitstudent.ac.in>
2023-10-30 21:51:42 +0530

    added some instruction for using it in local

--- Backend/llama_setup.py
+++ Backend/llama_setup.py
@@ -8,6 +8,7 @@ from llama_index.llms import LlamaCPP
 from llama_index.llms.llama_utils import messages_to_prompt, completion_to_prompt
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index import LangchainEmbedding
+
 #!CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install  llama-cpp-python --no-cache-dir
 #un comment this to use GPU engine- CUBLAS
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -27,13 +28,13 @@ else:
 
 # Setup LlamaCPP
 llm = LlamaCPP(
-    model_url='',
-    model_path='./dolphin-2.1-mistral-7b.Q4_K_M.gguf',
+    model_url='',  # compactible model is GGUF only.
+    model_path='./dolphin-2.1-mistral-7b.Q4_K_M.gguf', # Here I have use dolphin model from my local machine. please remove this and use your own model path
     temperature=0.1,
     max_new_tokens=3024,
     context_window=3900,
     generate_kwargs={},
-    model_kwargs={"n_gpu_layers": 70},
+    model_kwargs={"n_gpu_layers": 128},
     messages_to_prompt=messages_to_prompt,
     completion_to_prompt=completion_to_prompt,
     verbose=True,
